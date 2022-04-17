c225be823e46c4b9a568842ade2fce711bcee138
Anubhav Agarwal <anubhava@princeton.edu>
2022-04-17 19:59:15 -0400

    trying to get bert to work again

--- src/model.py
+++ src/model.py
@@ -169,9 +169,9 @@ class SAModel(nn.Module):
 
         # Pass through contextualizing component
         # print(self.bert)
-        res = type(self.bert(inputs_embeds=input_embs, attention_mask=masks, token_type_ids=segs)[0])
-        print(str(res))
-        output_bert = self.dropout(self.bert(inputs_embeds=input_embs, attention_mask=masks, token_type_ids=segs)[0])
+        # res = type(self.bert(inputs_embeds=input_embs, attention_mask=masks, token_type_ids=segs)[0])
+        # print(str(res))
+        output_bert = self.dropout(self.bert(inputs_embeds=input_embs, attention_mask=masks, token_type_ids=segs)[1])
         h = self.dropout(torch.tanh(self.linear_1(output_bert)))
         output = torch.sigmoid(self.linear_2(h)).squeeze(-1)
 
