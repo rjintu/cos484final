ffffa7b1d599a7ba6be8b37a3bc90564ead87c2e
Anubhav Agarwal <anubhava@princeton.edu>
2022-04-18 15:25:16 -0400

    we messed up again

--- src/model.py
+++ src/model.py
@@ -162,7 +162,7 @@ class SAModel(nn.Module):
             return bert_embs, input_embs
 
         # Pass through contextualizing component
-        output_bert = self.dropout(self.bert(inputs_embeds=input_embs, attention_mask=masks, token_type_ids=segs)[0])
+        output_bert = self.dropout(self.bert(inputs_embeds=input_embs, attention_mask=masks, token_type_ids=segs)[1])
         h = self.dropout(torch.tanh(self.linear_1(output_bert)))
         output = torch.sigmoid(self.linear_2(h)).squeeze(-1)
 
